2e6239d8ebc64f8ff13af7ceb4654da953df17ac
Ravindu Aratchige <109760834+ravi-aratchige@users.noreply.github.com>
2025-10-12 20:16:45 +0530

    UPDATE: add CORS and media device access

--- server/main.py
+++ server/main.py
@@ -1,13 +1,30 @@
+import os
 import uvicorn
 from fastapi import FastAPI
 from selenium import webdriver
+from fastapi.middleware.cors import CORSMiddleware
 from selenium.webdriver.chrome.options import Options
 
 # Initialize FastAPI application
 app = FastAPI()
 
-# Initialize extension and user profile settings
+# Define allowed origins
+origins = [
+    "http://localhost",
+]
+
+# Add CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Set extension and listener paths
 EXTENSION_PATH = "../extension"
+LISTENER_PATH = os.path.abspath("../extension/index.html")
 
 
 @app.get("/")
@@ -24,11 +41,24 @@ def main():
     # Initialize ChromeOptions
     options = Options()
 
+    # Set options to allow media device access
+    options.add_argument("--use-fake-ui-for-media-stream")
+    options.add_argument("--use-fake-device-for-media-stream")
+    prefs = {"profile.default_content_setting_values.media_stream_mic": 1}
+    options.add_experimental_option("prefs", prefs)
+
     # Load unpacked extension into browser
     options.add_argument(f"--load-extension={EXTENSION_PATH}")
 
     # Initialize Chromedriver
     driver = webdriver.Chrome(options=options)
+
+    # Open page for testing
+    driver.get(f"file:{LISTENER_PATH}")
+
+    # Open a new tabs
+    driver.switch_to.new_window("tab")
+    driver.get("https://example.com")
 
     # Start server
     uvicorn.run(
